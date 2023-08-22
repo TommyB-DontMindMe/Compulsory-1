8d14363883571b77635f0485d126bf6d1cd25175
Tommy <Vivikoro@live.no>
2023-08-22 14:00:45 +0200

    I think I maybe figured it out.

---
+++ Compulsory 1.cpp
@@ -0,0 +1,14 @@
+// This stuff tells the computer what stuff it needs to run the rest of this code.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// The purpose of this code is to demonstrate some basic understanding of programming.
+	// This next line outputs the given string
+	cout << "Hello to this terrible and wonderous world!";
+
+	// This line tells the program that it is finished.
+	return 0;
+
+}
